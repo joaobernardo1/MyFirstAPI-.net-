880113407e1fc7ddf2dc5cd79a4926a209054137
João Vitor Bernardo <153684355+joaobernardo1@users.noreply.github.com>
2025-01-28 23:25:24 -0300

    Second commit

---
+++ Controllers/UserController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers;
+[Route("[controller]")]
+
+[ApiController]
+public class UserController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok("João Vitor");
+    }
+}
