9be15710d2416dda22c8a9fec0cee61b3d2c4d96
João Vitor Bernardo <153684355+joaobernardo1@users.noreply.github.com>
2025-01-29 12:33:57 -0300

    Criação método Post

---
+++ Communication/Response/ResponseRegisterUserJson.cs
@@ -0,0 +1,7 @@
+﻿namespace WebApplication1.Communication.Response;
+
+public class ResponseRegisterUserJson
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+}
--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Communication.Requests;
+using WebApplication1.Communication.Response;
 
 namespace WebApplication1.Controllers;
 [Route("[controller]")]
@@ -8,8 +10,29 @@ namespace WebApplication1.Controllers;
 public class UserController : ControllerBase
 {
     [HttpGet]
-    public IActionResult Get()
+    [ProducesResponseType(typeof(User), StatusCodes.Status200OK)]
+    public IActionResult GetById([FromHeader]int id,[FromHeader] string? nickname)
     {
-        return Ok("João Vitor");
+        var response = new User
+        {
+            Age = 12,
+            Id = 1,
+            Name = "João"
+        };
+
+        return Ok(response);
+    }
+
+    [HttpPost]
+    [ProducesResponseType(typeof(ResponseRegisterUserJson), StatusCodes.Status201Created)]
+    public IActionResult Create([FromBody]RequestRegisterUserJson request)
+    {
+        var response = new ResponseRegisterUserJson
+        {
+            Id = 1,
+            Name = request.Name,
+        };
+
+        return Created(string.Empty, response);
     }
 }
